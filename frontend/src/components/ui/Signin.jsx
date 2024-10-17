6d485eb91c4d6fb18a5d831db3a79f8a382e09c4
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 23:17:42 +0530

    new

--- frontend/src/components/ui/Signin.jsx
+++ frontend/src/components/ui/Signin.jsx
@@ -20,7 +20,7 @@ const Signin = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "https://127.0.0.1:8000/api/login/",
+        "https://jobedinwebsite-production.up.railway.app/api/login/",
         {
           username: username,
           password: password,
